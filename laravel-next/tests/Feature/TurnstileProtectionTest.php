fbfb690a551230814ec6fcb1bf1e0d0c44fdc758
QuinnFreeze <QuinnFreeze@users.noreply.github.com>
2026-08-11 20:44:19 +0800

    test: exclude csrf from turnstile regression setup

--- laravel-next/tests/Feature/TurnstileProtectionTest.php
+++ laravel-next/tests/Feature/TurnstileProtectionTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
